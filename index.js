e9ee07e83c443093a94c453172020087d9b1832b
Tutul Chakma <tutulcou@gmail.com>
2025-05-30 20:12:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server);
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-// });
-
-// server.listen(3000, () => {
-//     console.log('listening on *:3000');
-// });
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -26,7 +6,7 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // or restrict to: "http://tutul-k8s-2.rfft.nl"
+    origin: "*", 
     methods: ["GET", "POST"]
   }
 });
